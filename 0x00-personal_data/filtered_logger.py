2f92eb9faa5ecf3d7c88b32896b2bd5e8e7878bc
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-08-08 17:51:52 +0100

    initial commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """
 Task:
-    Write a function called filter_datum that returns the log message obfuscated:
+    Write a function called filter_datum that returns the log
+      message obfuscated:
     Arguments:
         fields: a list of strings representing all fields to obfuscate
         redaction: a string representing by what the field will be obfuscated
         message: a string representing the log line
         separator: a string representing by which character is separating all
           fields in the log line (message)
-    The function should use a regex to replace occurrences of certain field values.
+    The function should use a regex to replace occurrences of certain
+      field values.
     filter_datum should be less than 5 lines long and use re.sub to perform
       the substitution with a single regex.
 """
@@ -27,7 +29,8 @@ from typing import List
 #     message = message.split(separator)
 #     li = []
 #     for data in message:
-#         li.append(f"{data.split('=')[0]}={redaction}" if data.split('=')[0] in fields else data)
+#         li.append(f"{data.split('=')[0]}={redaction}"
+#               if data.split('=')[0] in fields else data)
 #     return ';'.join(list(li))
 
 
@@ -37,8 +40,8 @@ from typing import List
 #     return re.sub(regex_pattern, f"{regex_pattern}={redaction}", message)
 
 
-def filter_datum(fields: List, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
     """ that returns the log message obfuscated """
     regex = re.compile(fr"({'|'.join(fields)})=[^{separator}]+")
-    print('regex >>> ' ,regex)
     return regex.sub(fr"\1={redaction}", message)
