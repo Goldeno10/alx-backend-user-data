1a6381394609d08d2bf662fa8c5bde973686d265
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-10-04 18:28:07 +0100

    ...

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -57,7 +57,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields=None):
         """init function"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields or []
